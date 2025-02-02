88138da523a9bbaf1ce7c66d1f5286d056399b08
Tyler Johnson <tsjnsn@gmail.com>
2025-02-01 23:08:48 -0600

    init neu

---
+++ client/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/web/index.html'),
+      },
+    },
+  },
+  server: {
+    port: 1420,
+  },
+}) 
